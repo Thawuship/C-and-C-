a8f9d8a498383251f8ba183241c7b6e707aebc06
Thawuship <49475559+Thawuship@users.noreply.github.com>
2021-05-29 15:04:21 +0530

    Add files via upload

---
+++ 02. Structure.cpp
@@ -0,0 +1,21 @@
+// Structure
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+// definition of the structure 
+struct Rectangle{
+	int length;
+	int breadth;
+};
+
+int main(){
+	struct Rectangle r1 = {5,6}; // This is the declaration and initializatino of r1 structure
+	r1.length = 3; // change the value of length
+	cout<<sizeof(r1)<<endl;
+	cout<<r1.breadth<<endl;
+	cout<<r1.length<<endl;
+}
+
+
